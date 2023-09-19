b93fa4165d3f0dc0a6469210fe5d1087ed780a42
Ahmed DotNet Dev <AhmedAdala@gmail.com>
2023-09-19 18:32:48 +0300

    Add pagination for browse

--- FileSharingAPP/Controllers/UploadsController.cs
+++ FileSharingAPP/Controllers/UploadsController.cs
@@ -12,156 +12,170 @@ using System.Threading.Tasks;
 
 namespace FileSharingAPP.Controllers
 {
-    [Authorize]
-    public class UploadsController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        private readonly IWebHostEnvironment env;
+	[Authorize]
+	public class UploadsController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly IWebHostEnvironment env;
 
-        public UploadsController(ApplicationDbContext context, IWebHostEnvironment env)
-        {
-            this._db = context;
-            this.env = env;
-        }
+		public UploadsController(ApplicationDbContext context, IWebHostEnvironment env)
+		{
+			this._db = context;
+			this.env = env;
+		}
 
-        public IActionResult Index()
-        {
-            var result = _db.uploads.Where(u => u.UserId == UserId)
-                .OrderByDescending(u => u.DownloadCount)
-                .Select(u => new UploadViewModel
-                {
-                    Id = u.Id,
-                    FileName = u.FileName,
-                    OriginalName = u.OriginalName,
-                    ContentType = u.ContentType,
-                    Size = u.Size,
-                    UploadDate = u.UploadDate,
-                    DownloadCount = u.DownloadCount,
-                });
-            return View(result);
-        }
-        private string UserId
-        {
-            get
-            {
-                return User.FindFirstValue(ClaimTypes.NameIdentifier);
-            }
-        }
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			var result = _db.uploads.Where(u => u.UserId == UserId)
+				.OrderByDescending(u => u.DownloadCount)
+				.Select(u => new UploadViewModel
+				{
+					Id = u.Id,
+					FileName = u.FileName,
+					OriginalName = u.OriginalName,
+					ContentType = u.ContentType,
+					Size = u.Size,
+					UploadDate = u.UploadDate,
+					DownloadCount = u.DownloadCount,
+				});
+			return View(result);
+		}
+		private string UserId
+		{
+			get
+			{
+				return User.FindFirstValue(ClaimTypes.NameIdentifier);
+			}
+		}
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Create(InputUpload model)
-        {
-            if (ModelState.IsValid)
-            {
-                var NewName = Guid.NewGuid().ToString();
-                var Extension = Path.GetExtension(model.File.FileName);
-                var FileName = string.Concat(NewName, Extension);
-                var Root = env.WebRootPath;
-                var path = Path.Combine(Root, "Uploads", FileName);
+		[HttpPost]
+		public async Task<IActionResult> Create(InputUpload model)
+		{
+			if (ModelState.IsValid)
+			{
+				var NewName = Guid.NewGuid().ToString();
+				var Extension = Path.GetExtension(model.File.FileName);
+				var FileName = string.Concat(NewName, Extension);
+				var Root = env.WebRootPath;
+				var path = Path.Combine(Root, "Uploads", FileName);
 
-                using (var fs = System.IO.File.Create(path))
-                {
-                    await model.File.CopyToAsync(fs);
-                }
-                await _db.uploads.AddAsync(new Uploads
-                {
-                    OriginalName = model.File.FileName,
-                    FileName = FileName,
-                    ContentType = model.File.ContentType,
-                    Size = model.File.Length,
-                    UserId = UserId,
-                });
-                await _db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(model);
-        }
-        [HttpGet]
-        public async Task<IActionResult> Delete(string id)
-        {
-            var SelectedUpload = await _db.uploads.FindAsync(id);
-            if (SelectedUpload == null)
-            {
-                return NotFound();
-            }
-            if (SelectedUpload.UserId != UserId)
-            {
-                return NotFound();
-            }
-            ViewBag.UploadId = id;
-            return View(SelectedUpload);
-        }
+				using (var fs = System.IO.File.Create(path))
+				{
+					await model.File.CopyToAsync(fs);
+				}
+				await _db.uploads.AddAsync(new Uploads
+				{
+					OriginalName = model.File.FileName,
+					FileName = FileName,
+					ContentType = model.File.ContentType,
+					Size = model.File.Length,
+					UserId = UserId,
+				});
+				await _db.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(model);
+		}
+		[HttpGet]
+		public async Task<IActionResult> Delete(string id)
+		{
+			var SelectedUpload = await _db.uploads.FindAsync(id);
+			if (SelectedUpload == null)
+			{
+				return NotFound();
+			}
+			if (SelectedUpload.UserId != UserId)
+			{
+				return NotFound();
+			}
+			ViewBag.UploadId = id;
+			return View(SelectedUpload);
+		}
 
-        [ActionName("Delete")]
-        [HttpPost]
-        public async Task<IActionResult> DeleteConfirmation(string id)
-        {
+		[ActionName("Delete")]
+		[HttpPost]
+		public async Task<IActionResult> DeleteConfirmation(string id)
+		{
 
-            var SelectedUpload = await _db.uploads.FindAsync(id);
-            if (SelectedUpload == null)
-            {
-                return NotFound();
-            }
+			var SelectedUpload = await _db.uploads.FindAsync(id);
+			if (SelectedUpload == null)
+			{
+				return NotFound();
+			}
 
-            _db.uploads.Remove(SelectedUpload);
-            await _db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        public IActionResult Results(string term)
-        {
-            var model = _db.uploads.Where(p => p.OriginalName.Contains(term))
-                .Select(u => new UploadViewModel
-                {
-                    FileName = u.FileName,
-                    OriginalName = u.OriginalName,
-                    ContentType = u.ContentType,
-                    Size = u.Size,
-                    UploadDate = u.UploadDate,
-                    DownloadCount = u.DownloadCount,
+			_db.uploads.Remove(SelectedUpload);
+			await _db.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+		[HttpPost]
+		[AllowAnonymous]
+		public IActionResult Results(string term)
+		{
+			var model = _db.uploads.Where(p => p.OriginalName.Contains(term))
+				.Select(u => new UploadViewModel
+				{
+					FileName = u.FileName,
+					OriginalName = u.OriginalName,
+					ContentType = u.ContentType,
+					Size = u.Size,
+					UploadDate = u.UploadDate,
+					DownloadCount = u.DownloadCount,
 
-                });
+				});
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult Browse()
-        {
-            var model = _db.uploads.OrderByDescending(p => p.UploadDate)
-                .Select(u => new UploadViewModel
-                {
-                    FileName = u.FileName,
-                    OriginalName = u.OriginalName,
-                    ContentType = u.ContentType,
-                    Size = u.Size,
-                    UploadDate = u.UploadDate,
-                });
-            return View(model);
-        }
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> Downloads(string id)
-        {
-            var SelectedFile = await _db.uploads.FirstOrDefaultAsync(p => p.FileName == id);
-            if (SelectedFile == null)
-            {
-                return NotFound();
-            }
-            SelectedFile.DownloadCount++;
-            _db.Update(SelectedFile);
-            await _db.SaveChangesAsync();
-            var Path = "~/Uploads/" + SelectedFile.FileName;
-            Response.Headers.Add("Expires", DateTime.Now.AddDays(-3).ToLongDateString());
-            Response.Headers.Add("Cache-Control", "no-cache");
-            return File(Path, SelectedFile.ContentType, SelectedFile.OriginalName);
-        }
-    }
+		[HttpGet]
+		[AllowAnonymous]
+		public async Task<IActionResult> Browse(int RequiredPage = 1)
+		{
+			const int PageSize = 3;
+			int SkipCount = (RequiredPage - 1) * PageSize;
+			decimal RowsCount = await _db.uploads.CountAsync();
+			var PagesCount = Math.Ceiling(RowsCount / PageSize);
+			if (RequiredPage > PagesCount)
+			{
+				RequiredPage = 1;
+			}
+			RequiredPage = RequiredPage <= 0 ? 1 : RequiredPage;
+			var model = await _db.uploads
+				.OrderByDescending(p => p.DownloadCount)
+				.Select(u => new UploadViewModel
+				{
+					FileName = u.FileName,
+					OriginalName = u.OriginalName,
+					ContentType = u.ContentType,
+					Size = u.Size,
+					UploadDate = u.UploadDate,
+				})
+				.Skip(SkipCount)
+				.Take(PageSize).ToListAsync();
+			ViewBag.CurrentPage = RequiredPage;
+			ViewBag.PagesCount = PagesCount;
+			return View(model);
+		}
+		[HttpGet]
+		[AllowAnonymous]
+		public async Task<IActionResult> Downloads(string id)
+		{
+			var SelectedFile = await _db.uploads.FirstOrDefaultAsync(p => p.FileName == id);
+			if (SelectedFile == null)
+			{
+				return NotFound();
+			}
+			SelectedFile.DownloadCount++;
+			_db.Update(SelectedFile);
+			await _db.SaveChangesAsync();
+			var Path = "~/Uploads/" + SelectedFile.FileName;
+			Response.Headers.Add("Expires", DateTime.Now.AddDays(-3).ToLongDateString());
+			Response.Headers.Add("Cache-Control", "no-cache");
+			return File(Path, SelectedFile.ContentType, SelectedFile.OriginalName);
+		}
+	}
 }
--- FileSharingAPP/Program.cs
+++ FileSharingAPP/Program.cs
@@ -1,26 +1,32 @@
+using FileSharingAPP.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FileSharingAPP
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
+			//migration at runtime env
+			using (var scope = host.Services.CreateScope())
+			{
+				var provider = scope.ServiceProvider;
+				var DbContext = provider.GetRequiredService<ApplicationDbContext>();
+				DbContext.Database.Migrate();
+				//seed
+			}
+			host.Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }

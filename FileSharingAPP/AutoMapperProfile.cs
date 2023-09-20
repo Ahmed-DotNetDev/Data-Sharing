6a8a314fa13026fd190d5ab756df61cb5d080354
Ahmed DotNet Dev <AhmedAdala@gmail.com>
2023-09-20 21:31:30 +0300

    Implement auto mapper

---
+++ FileSharingAPP/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace FileSharingAPP
+{
+    public class UploadProfile : Profile
+    {
+        public UploadProfile()
+        {
+            CreateMap<Models.InputUpload, Data.Uploads>();
+            CreateMap<Data.Uploads, Data.Uploads>();
+        }
+    }
+}
